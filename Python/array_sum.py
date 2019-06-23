5816f567e8e465d58970687871d79482d91c1818
udaywarier <uday.warier1@gmail.com>
2019-06-23 19:10:09 -0400

    DCP #1

---
+++ Python/array_sum.py
@@ -0,0 +1,27 @@
+def array_sum_exists(k, arr):
+    complements = dict()
+
+    for i in range(len(arr)):
+        complement = k - arr[i]
+
+        if complement > 0:
+            if arr[i] in complements.keys():
+                return True
+            else:
+                complements[complement] = arr[i]
+    
+    return False
+        
+input_arr = [10, 5, 3, 7]
+k = 17
+
+print(array_sum_exists(k, input_arr)) # true
+
+k = 16
+print(array_sum_exists(k, input_arr)) # false
+
+k = 10
+print(array_sum_exists(k, input_arr)) # true
+
+k = 5
+print(array_sum_exists(k, input_arr)) # false
