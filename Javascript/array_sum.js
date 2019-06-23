7c88666a2a40374ffe5da3e2accde79ab85cdb6a
udaywarier <uday.warier1@gmail.com>
2019-06-23 18:47:38 -0400

    DCP #1

---
+++ Javascript/array_sum.js
@@ -0,0 +1,38 @@
+let input_array = [10, 15, 3, 7];
+let k = 17;
+
+console.log(array_sum_exists(k, input_array)); //true
+
+k = 16;
+console.log(array_sum_exists(k, input_array)); //false
+
+k = 10;
+console.log(array_sum_exists(k, input_array)); //true
+
+k = 15;
+console.log(array_sum_exists(k, input_array)); //false
+
+function array_sum_exists(k, arr)
+{
+    let complements = [];
+
+    for(let i = 0; i < arr.length; i++)
+    {
+        let complement = k - arr[i];
+
+        if(complement > 0)
+        {
+            if(complements[arr[i]] != undefined)
+            {
+                return true;
+            }
+
+            else
+            {
+                complements[complement] = arr[i];
+            }
+        }
+    }
+
+    return false;
+}
