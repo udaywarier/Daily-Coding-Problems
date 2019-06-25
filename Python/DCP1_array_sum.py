bc1fe2126b7fac1b2dd68e4a024f2ea8543e0fc3
Uday Warier <32044538+udaywarier@users.noreply.github.com>
2019-06-25 11:15:23 -0400

    Renamed array_sum.py to DCP1_array_sum.py

--- Python/DCP1_array_sum.py
+++ Python/DCP1_array_sum.py
@@ -1,20 +1,17 @@
 def array_sum_exists(k, arr):
     complements = dict()
-
     for i in range(len(arr)):
         complement = k - arr[i]
-
         if complement > 0:
             if arr[i] in complements:
                 return True
             else:
                 complements[complement] = arr[i]
-    
     return False
         
 input_arr = [10, 5, 3, 7]
-k = 17
 
+k = 17
 print(array_sum_exists(k, input_arr)) # true
 
 k = 16
