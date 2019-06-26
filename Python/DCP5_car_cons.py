d4cceb45d60a72293fa07c8d8f9135cae7fd6b03
Uday Warier <32044538+udaywarier@users.noreply.github.com>
2019-06-26 08:46:27 -0400

    Updated the car and cdr functions

--- Python/DCP5_car_cons.py
+++ Python/DCP5_car_cons.py
@@ -3,15 +3,15 @@ def cons(a, b):
         return f(a, b)
     return pair
 
-def car(pair):
+def car(cons):
   def get_first(a, b):
     return a
-  return pair(get_first)
+  return cons(get_first)
 
-def cdr(pair):
+def cdr(cons):
   def get_last(a, b):
     return b
-  return pair(get_last)
+  return cons(get_last)
 
 print(car(cons(3, 4))) # 3
 print(cdr(cons(3, 4))) # 4
